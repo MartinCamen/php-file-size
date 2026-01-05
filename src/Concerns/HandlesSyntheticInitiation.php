3727cf5518354823909c81a7872a94e5cc31934d
Martin Camenius <camen@camensky.se>
2026-01-05 02:18:39 +0100

    Return static

--- src/Concerns/HandlesSyntheticInitiation.php
+++ src/Concerns/HandlesSyntheticInitiation.php
@@ -5,50 +5,50 @@ namespace MartinCamen\PhpFileSize\Concerns;
 trait HandlesSyntheticInitiation
 {
     /** @param array<string, mixed> $options */
-    public static function fromBytes(int|float $value, array $options = []): self
+    public static function fromBytes(int|float $value, array $options = []): static
     {
-        return self::initiate($options)->bytes($value);
+        return static::initiate($options)->bytes($value);
     }
 
     /** @param array<string, mixed> $options */
-    public static function fromKilobytes(int|float $value, array $options = []): self
+    public static function fromKilobytes(int|float $value, array $options = []): static
     {
-        return self::initiate($options)->kilobytes($value);
+        return static::initiate($options)->kilobytes($value);
     }
 
     /** @param array<string, mixed> $options */
-    public static function fromMegabytes(int|float $value, array $options = []): self
+    public static function fromMegabytes(int|float $value, array $options = []): static
     {
-        return self::initiate($options)->megabytes($value);
+        return static::initiate($options)->megabytes($value);
     }
 
     /** @param array<string, mixed> $options */
-    public static function fromGigabytes(int|float $value, array $options = []): self
+    public static function fromGigabytes(int|float $value, array $options = []): static
     {
-        return self::initiate($options)->gigabytes($value);
+        return static::initiate($options)->gigabytes($value);
     }
 
     /** @param array<string, mixed> $options */
-    public static function fromTerabytes(int|float $value, array $options = []): self
+    public static function fromTerabytes(int|float $value, array $options = []): static
     {
-        return self::initiate($options)->terabytes($value);
+        return static::initiate($options)->terabytes($value);
     }
 
     /** @param array<string, mixed> $options */
-    public static function fromPetabytes(int|float $value, array $options = []): self
+    public static function fromPetabytes(int|float $value, array $options = []): static
     {
-        return self::initiate($options)->petabytes($value);
+        return static::initiate($options)->petabytes($value);
     }
 
     /** @param array<string, mixed> $options */
-    public static function zero(array $options = []): self
+    public static function zero(array $options = []): static
     {
-        return self::initiate($options)->bytes(0);
+        return static::initiate($options)->bytes(0);
     }
 
     /** @param array<string, mixed> $options */
-    private static function initiate(array $options = []): self
+    private static function initiate(array $options = []): static
     {
-        return new self(options: $options);
+        return new static(options: $options);
     }
 }
